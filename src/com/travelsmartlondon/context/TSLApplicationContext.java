1d96402a252e3f9ebcf235ada634462308385376
kamilprzekwas <kamilprzekwas@gmail.com>
2013-02-27 15:53:29 +0000

    Created Boundary and Point classes to determine which database to load at any given point

--- src/com/travelsmartlondon/context/TSLApplicationContext.java
+++ src/com/travelsmartlondon/context/TSLApplicationContext.java
@@ -1,5 +1,10 @@
 package com.travelsmartlondon.context;
 
+import java.util.Map;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * TSLApplicationContext singleton context object.
  * <p>Stores session-related cached information</p>
@@ -8,8 +13,9 @@ package com.travelsmartlondon.context;
  */
 public class TSLApplicationContext {
 
-	private String _userEmailAddress;
+	private String _emailAddress;
 	
+	//public static final String USER_EMAIL_ADDRESS = "username";
 	
 	private static final TSLApplicationContext INSTANCE = new TSLApplicationContext();
 	
@@ -22,16 +28,80 @@ public class TSLApplicationContext {
 		return INSTANCE;
 	}
 	
-	
 	public String currentEmailAddress() {
-		return this._userEmailAddress;
+		return this._emailAddress;
 	}
 	
 	public boolean checkIfUserIsLoggedIn() {
-		return (this._userEmailAddress == null) ? false : true;
+		return (this._emailAddress == null) ? false : true;
 	}
 	
 	public void injectEmailAddressOnceUserLoggedIn(String email_) {
-		this._userEmailAddress = email_;
+		this._emailAddress = email_;
 	}
+
+	/*
+	private Map<String, Object> _preferenceMap = new ConcurrentHashMap<String, Object>(); 
+	
+	@Override
+	public boolean contains(String preference_) {
+		return this._preferenceMap.containsKey(preference_);
+	}
+
+	@Override
+	public Editor edit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, ?> getAll() {
+		return this._preferenceMap;
+	}
+
+	@Override
+	public boolean getBoolean(String key_, boolean defValue_) {
+		return (this._preferenceMap.containsKey(key_) ? (Boolean) this._preferenceMap.get(key_) : defValue_);
+	}
+
+	@Override
+	public float getFloat(String key_, float defValue_) {
+		return (this._preferenceMap.containsKey(key_) ? (Float) this._preferenceMap.get(key_) : defValue_);
+	}
+
+	@Override
+	public int getInt(String key_, int defValue_) {
+		return (this._preferenceMap.containsKey(key_) ? (Integer) this._preferenceMap.get(key_) : defValue_);
+	}
+
+	@Override
+	public long getLong(String key_, long defValue_) {
+		return (this._preferenceMap.containsKey(key_) ? (Long) this._preferenceMap.get(key_) : defValue_);
+	}
+
+	@Override
+	public String getString(String key_, String defValue_) {
+		return (this._preferenceMap.containsKey(key_) ? (String) this._preferenceMap.get(key_) : defValue_);
+	}
+
+	@Override
+	public Set<String> getStringSet(String key_, Set<String> defValue_) {
+		Set<String> set = (Set<String>) this._preferenceMap.get(key_);
+		return (this._preferenceMap.containsKey(key_) ? set : defValue_);
+	}
+
+	@Override
+	public void registerOnSharedPreferenceChangeListener(
+			OnSharedPreferenceChangeListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unregisterOnSharedPreferenceChangeListener(
+			OnSharedPreferenceChangeListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+	*/
 }
---
+++ src/com/travelsmartlondon/help/Boundary.java
@@ -0,0 +1,32 @@
+package com.travelsmartlondon.help;
+
+/**
+*
+* @author kamilprzekwas
+*/
+class Boundary {
+   private final Point[] _points; // Points making up the boundary
+
+   public Boundary(final Point[] points_) {
+       this._points = points_;
+   }
+   /**
+    * Return true if the given point is contained inside the boundary.
+    * See: http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+    * @param test The point to check
+    * @return true if the point is inside the boundary, false otherwise
+    *
+    */
+   public boolean contains(Point test) {
+     int i;
+     int j;
+     boolean result = false;
+     for (i = 0, j = this._points.length - 1; i < this._points.length; j = i++) {
+       if ((this._points[i].getY() > test.getY()) != (this._points[j].getY() > test.getY()) &&
+           (test.getX() < (this._points[j].getX() - this._points[i].getX()) * (test.getY() - this._points[i].getY()) / (this._points[j].getY()-this._points[i].getY()) + this._points[i].getX())) {
+         result = !result;
+        }
+     }
+     return result;
+   }
+}
